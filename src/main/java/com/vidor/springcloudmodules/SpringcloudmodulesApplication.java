3769bfeda64558bb8e7663ce2408b012a7fdb2c9
TS.SZX.chenwenwu <chenwenwu@travelsky.com>
2020-05-08 15:40:20 +0800

    SpringCloud父工程初始化

---
+++ src/main/java/com/vidor/springcloudmodules/SpringcloudmodulesApplication.java
@@ -0,0 +1,13 @@
+package com.vidor.springcloudmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudmodulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudmodulesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vidor/springcloudmodules/SpringcloudmodulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidor.springcloudmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudmodulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
